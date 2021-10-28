7017dc78cf496f15a63d7f24af1e4e7f92da6f41
Juliette Orpen <53294140+julietteorpen@users.noreply.github.com>
2021-10-28 18:23:47 +0200

    Create 02.js

---
+++ 02.js
@@ -0,0 +1,35 @@
+
+function transform(arg1) {
+  let result = [];
+  for (let i =0; i < arg1.length; i++){
+    if (arg1[i] % 3 !== 0) { 
+    result.push(arg1[i]);
+} 
+} 
+return result 
+}
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+console.log(transform([2,3,3,4]));
