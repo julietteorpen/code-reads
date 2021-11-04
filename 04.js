f6a47038f8ff22101728d9260282548018a1a4a6
Juliette Orpen <53294140+julietteorpen@users.noreply.github.com>
2021-11-04 15:00:45 +0000

    Update 04.js

--- 04.js
+++ 04.js
@@ -1,6 +1,10 @@
 function transform (arg1,arg2) {
-let newArr = new Array(arg1.length);
-return newArr.fill(arg2);
+let newArr = new Array(arg1.length)
+if (arg1.length > arg2.length) {
+ newArr.fill(arg2);
+} else {
+  return newArr;
+}
 }
 /
 /
